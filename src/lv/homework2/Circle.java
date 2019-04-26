f11b2b904ff448efd484a942e1c7f94058ecb0bb
Oskars Vilitis <oskars.vilitis@gmail.com>
2019-04-26 22:01:28 +0300

    Otrais mājasdarbs

---
+++ src/lv/homework2/Circle.java
@@ -0,0 +1,23 @@
+package lv.homework2;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+
+    }
+
+}
---
+++ src/lv/homework2/CircleTest.java
@@ -0,0 +1,16 @@
+package lv.homework2;
+
+public class CircleTest {
+
+    public static void main(String[] args) {
+        Circle megaAplis = new Circle();
+
+        megaAplis.setRadius(5);
+
+        megaAplis.calculateArea();
+
+        System.out.println(megaAplis.calculateArea());
+
+    }
+
+}
