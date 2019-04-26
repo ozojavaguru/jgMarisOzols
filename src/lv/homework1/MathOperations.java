337fe3bcf687a9f81172f9c423538058efa0c7c5
Oskars Vilitis <oskars.vilitis@gmail.com>
2019-04-26 20:22:52 +0300

    Pirmais mājasdarbs ar visām operacijam

---
+++ src/lv/homework1/MathOperations.java
@@ -0,0 +1,19 @@
+package lv.homework1;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int sumResult = firstNumber + secondNumber;
+        int subtractResult = firstNumber - secondNumber;
+        int divideResult = firstNumber / secondNumber;
+        int multiplyResult = firstNumber * secondNumber;
+        System.out.println("Sum result: " + sumResult);
+        System.out.println("Subtract result: " + subtractResult);
+        System.out.println("Divide result: " + divideResult);
+        System.out.println("Multiply result: " + multiplyResult);
+    }
+
+
+}
